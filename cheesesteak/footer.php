a1f9c4f42874e0d5707e3aa4f02b501e48e2b439
Rashim Bhandari <122297196+pampuu@users.noreply.github.com>
2024-10-17 23:02:54 +0545

    Delete cheesesteak directory

--- cheesesteak/footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-    <div class="footer-content">
-        <p>&copy; 2024 The CheeseSteak Factory. All Rights Reserved.</p>
-        <p>Contact us at <a href="mailto:info@cheesefactory.com">info@cheesefactory.com</a></p>
-    </div>
-</footer>
-</body>
-</html>
--- cheesesteak/includes/footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-    <div class="footer-content">
-        <p>&copy; 2024 Cheese Factory. All Rights Reserved.</p>
-        <p>Contact us at <a href="mailto:info@cheesefactory.com">info@cheesefactory.com</a></p>
-    </div>
-</footer>
-</body>
-</html>
